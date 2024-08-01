2d39ec49829f825dbe8d91b87ea63924c76ccd8b
Eduardo Alvarado Cortes (International Supplier) <v-eduardoal@microsoft.com>
2024-08-01 15:24:27 -0600

    Adjustments for Cosmos

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const User = require('./users');
 const database = require('./db');
 
+  
 const app = express();
 app.use(express.json());
 database.connect();
-
+//cosmos.connect();
 app.get('/users', async (req, res) => {
     try{
          const users = await User.find();
+
         res.json(users);
     } catch(err){
         res.status(500).json({message: err.message});
--- db.js
+++ db.js
@@ -1,8 +1,50 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+const { KeyClient } = require("@azure/keyvault-keys");
+const { DefaultAzureCredential } = require("@azure/identity");
 
-exports.connect = () =>{
-    mongoose.connect('mongodb://0.0.0.0:27017/academydb').then(()=> console.log('Mongodb connected...'))
-.catch(err => console.log(err));
+
+const credential = new DefaultAzureCredential();
+
+// const connstr = `mongodb://${process.env.COSMOSDB_HOST}:${process.env.COSMOSDB_PORT}/${process.env.COSMOSDB_DBNAME}?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000`;
+
+const endpoint = process.env.COSMOSDB_ENDPOINT;
+const key = process.env.COSMOSDB_KEY;
+const client = new CosmosClient({endpoint,key})
+
+async function main(){
+
+ const { database } = await client.databases.createIfNotExists({ id: "schooldb" });
+ console.log(database.id);
+
+ const { container } = await database.containers.createIfNotExists({ id: "teachers" });
+ console.log(container.id);
+
+ const teachers = [
+    { id: "1", name: "Eduardo", state: "WA", isCapitol: true },
+    { id: "2", name: "Jose", state: "WA", isCapitol: false },
+    { id: "3", name: "Francisco", state: "IL", isCapitol: false }
+  ];
+  for (const teacher of teachers) {
+    await container.items.create(teacher);
+  }
+
 
 }
+
+main().catch((error) => {
+    console.error(error);
+});
+// exports.connect = () => {
+//     mongoose.connect(connstr, {
+
+//     auth: {
+//         username: process.env.COSMOSDB_USER,
+//         password: process.env.COSMOSDB_PASSWORD
+//       },
+//     })
+//       .then(()=> console.log('Mongodb connected...'))
+//     .catch(err => console.log(err));
+// }  
  
